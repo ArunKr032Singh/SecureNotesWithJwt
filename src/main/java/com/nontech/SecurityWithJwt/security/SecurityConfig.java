0146239fff36291ac4c0e3e1955225fdac4f76ea
ArunKr032Singh <arunkrasingh@gmail.com>
2025-06-28 13:19:43 +0530

    add password encoder

--- src/main/java/com/nontech/SecurityWithJwt/security/SecurityConfig.java
+++ src/main/java/com/nontech/SecurityWithJwt/security/SecurityConfig.java
@@ -19,6 +19,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.LocalDate;
@@ -44,9 +46,14 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Bean
-    public CommandLineRunner initData(RoleRepo roleRepository, UserRepo userRepository) {
+    public CommandLineRunner initData(RoleRepo roleRepository, UserRepo userRepository,PasswordEncoder passwordEncoder) {
         return args -> {
             Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
                     .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_USER)));
@@ -55,7 +62,7 @@ public class SecurityConfig {
                     .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
 
             if (!userRepository.existsByUserName("user")) {
-                User user1 = new User("user", "user@example.com", "{noop}0000");
+                User user1 = new User("user", "user@example.com", passwordEncoder.encode("0000"));
                 user1.setAccountNonLocked(false);
                 user1.setAccountNonExpired(true);
                 user1.setCredentialsNonExpired(true);
@@ -69,7 +76,7 @@ public class SecurityConfig {
             }
 
             if (!userRepository.existsByUserName("admin")) {
-                User admin = new User("admin", "admin@example.com", "{noop}1111");
+                User admin = new User("admin", "admin@example.com", passwordEncoder.encode("1111"));
                 admin.setAccountNonLocked(true);
                 admin.setAccountNonExpired(true);
                 admin.setCredentialsNonExpired(true);
